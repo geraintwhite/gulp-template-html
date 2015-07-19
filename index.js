c67059b50feadf6cd59f0aadbe3567856066ccca
Geraint White <geraintwhite@gmail.com>
2015-07-19 20:46:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var through = require('through2'),
+    gutil = require('gulp-util'),
+    Templator = require('template-html'),
+    PluginError = gutil.PluginError;
+
+
+const PLUGIN_NAME = 'gulp-template-html';
+
+function gulpTemplator (options) {
+
+  var templator = new Templator(options);
+
+  function process (file, enc, cb) {
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams aren\'t supported'));
+    }
+
+    if (file.isBuffer()) {
+      content = templator.processFile(file.path);
+      file.contents = new Buffer(content, enc);
+    }
+
+    cb(null, file);
+  }
+
+  return through.obj(process);
+}
+
+module.exports = gulpTemplator;
